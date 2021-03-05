c9ca69eaabec04a4276118fc236b286ce5822e0a
Hoang Hung <hth321@gmail.com>
2021-03-05 11:08:18 +0700

    add typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
+
 import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
@@ -37,8 +39,7 @@ export default function Home({ allPostsData }) {
     </Layout>
   )
 }
-
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async context => {
   const allPostsData = getSortedPostsData()
   return {
     props: {
@@ -46,3 +47,4 @@ export async function getStaticProps() {
     }
   }
 }
+ 
